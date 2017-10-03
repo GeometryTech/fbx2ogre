0a9747c05857df097b66197eedb60a3419d0cb6c
BinFu <jupiter.ben@live.com>
2017-10-03 11:01:31 +0800

    add deps lib cmake

--- build.bat
+++ build.bat
@@ -3,12 +3,14 @@ set WinDev=%~dp0../../WinDev
 set CMakeDir=%WinDev%/cmake/3.9.3/x64
 set path=%CMakeDir%/bin;%path%
 
-set FBX_ROOT="D:\Source\fbx-sdk"
+set FBX_ROOT="D:\Source\fbxsdk"
+set TinyXML_ROOT_DIR=""
+set OGRE_SDK=""
 
 cd %~dp0
 mkdir build
 cd build
 
-cmake -G "Visual Studio 14 2015 Win64" ..
+cmake --trace -G "Visual Studio 14 2015 Win64" ..
 cd ..
 cmake --build build/vs2015
